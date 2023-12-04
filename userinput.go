9ddc77001eeefbc785ab2cbc5169871f131a0723
code-farmhouse <code.farmhouse@gmail.com>
2023-12-04 09:52:38 -0800

    built function to retrieve year

--- userinput.go
+++ userinput.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"errors"
+	"fmt"
 	"os"
+	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 func main() {
-	fmt.Println("Please select a question #: ")
+	chosenYear := ""
+	err := errors.New("")
+	for {
+		chosenYear, err = retrieveValidYear()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		break
+	}
 
+	fmt.Println(fmt.Sprintf("You have selected year: %v", chosenYear))
+}
+
+func retrieveValidYear() (string, error) {
 	// read input using go std lib bufio, getting input from cmdline
 	reader := bufio.NewReader(os.Stdin)
 
-	// read in the user's selected question number
-	questionNumber, err := reader.ReadString('\n')
+	// get the appropriate year from the user
+	possibleYears := []string{"2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"}
+	fmt.Println("Please select a year : ")
+	selectedYear, err := reader.ReadString('\n')
 	if err != nil {
-		panic(err)
+		return "", errors.New("Could not read from command line. Please retry with valid input.")
 	}
-	
-	message := fmt.Sprintf("You selected question %v", questionNumber)
-	fmt.Println(message)
+
+	selectedYear = strings.TrimSuffix(selectedYear, "\n")
+
+	if !slices.Contains(possibleYears, selectedYear) {
+		return "", errors.New(fmt.Sprintf("Not a valid year. Please retry from the following options: %v", possibleYears))
+	}
+
+	return selectedYear, nil
 }
